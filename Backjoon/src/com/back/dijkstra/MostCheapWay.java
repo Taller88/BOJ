a4b2b23438f2c1357efe79d28964e5e8c12c2454
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2020-08-11 19:15:46 +0900

    백준 다익스트라 2문제 문제풀이

--- Backjoon/src/com/back/dijkstra/MostCheapWay.java
+++ Backjoon/src/com/back/dijkstra/MostCheapWay.java
@@ -8,80 +8,76 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class ShortestWay {
-
+public class MostCheapWay {
 	static class Node implements Comparable<Node>{
 		int end, weight;
+		
 		public Node(int end, int weight) {
 			this.end=end;
 			this.weight=weight;
 		}
+		
+		
 		@Override
 		public int compareTo(Node o) {
 			return this.weight-o.weight;
 		}
-		
 	}
-	private final static int INF=100_000_000;
+	private final static int INF=2_147_483_647;
 	static int[]dist;
 	static List<Node>[] list;
 	public static void main(String[] args) throws Exception{
-//		5 6
-//		1
-//		5 1 1
+//		5
+//		8
 //		1 2 2
 //		1 3 3
-//		2 3 4
-//		2 4 5
-//		3 4 6
-
+//		1 4 1
+//		1 5 10
+//		2 4 2
+//		3 4 1
+//		3 5 1
+//		4 5 3
+//		1 5
+		
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		int num=Integer.parseInt(st.nextToken());
-		int vex=Integer.parseInt(st.nextToken());
-		int k=Integer.parseInt(br.readLine());
-		dist=new int[num+1];
-		list=new ArrayList[num+1];
+		int cityNum=Integer.parseInt(br.readLine());
+		int busNum=Integer.parseInt(br.readLine());
+		dist=new int[cityNum+1];
+		list=new ArrayList[cityNum+1];
+		StringTokenizer st;
 		Arrays.fill(dist, INF);
-		for(int i=1 ;i<=num; i++) {
+		for(int i=1; i<=cityNum; i++) {
 			list[i]=new ArrayList<Node>();
 		}
-		
-		
-		for(int i=0; i<vex; i++) {
+		for(int i=0; i<busNum; i++) {
 			st=new StringTokenizer(br.readLine());
 			int start=Integer.parseInt(st.nextToken());
 			int end=Integer.parseInt(st.nextToken());
 			int weight=Integer.parseInt(st.nextToken());
-			list[start].add(new Node(end, weight));
+			list[start].add(new Node(end,weight));
 		}
 		
-		dijkstra(k);
-		StringBuilder sb=new StringBuilder();
-		for(int i=1; i<dist.length; i++) {
-			if(dist[i]==INF) {
-				sb.append("INF\n");
-			}else {
-				sb.append(dist[i]+"\n");
-			}
-		}
-		System.out.println(sb.toString().trim());
+		st=new StringTokenizer(br.readLine());
+		int k=Integer.parseInt(st.nextToken());
+		int end=Integer.parseInt(st.nextToken());
 		
+		dijkstra(k,end);
+		System.out.println(dist[end]);
 		
 	}
-	public static void dijkstra(int k) {
+	public  static void dijkstra(int k, int end) {
 		PriorityQueue<Node> pq=new PriorityQueue<>();
 		dist[k]=0;
 		pq.add(new Node(k,0));
 		while(!pq.isEmpty()) {
-			Node n=pq.poll();
-			for(Node node: list[n.end]) {
-				if(dist[node.end]>dist[n.end]+node.weight) {
-					dist[node.end]=dist[n.end]+node.weight;
-					pq.add(new Node(node.end,dist[node.end]));
-				}
-			}
+			Node node=pq.poll();
 			
+			for(Node n: list[node.end]) {
+				if(dist[n.end]>dist[node.end]+n.weight) {
+					dist[n.end]=dist[node.end]+n.weight;
+					pq.add(new Node(n.end,dist[n.end]));
+				}
+			}			
 		}
 	}
 
