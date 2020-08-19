cb13b937e8e97b932be3ae2426cde71c8242eec5
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2020-08-19 19:32:10 +0900

    백준 브루트포스 체스판 다시 칠하기 문제풀이

--- Backjoon/src/com/back/bruteforce/ChessPrint.java
+++ Backjoon/src/com/back/bruteforce/ChessPrint.java
@@ -4,53 +4,81 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-import com.back.dfsbfs.DfsBfs;
-
 public class ChessPrint {
-	static int[] dx= {1,-1,0,0};
-	static int[] dy= {0,0,1,-1};
-	static int min=0;
-	public static void main(String[] args) throws Exception{
+	static String[][]arr;
+	static int answer=Integer.MAX_VALUE;
+	public static void check(String[][]arr) {
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[0].length; j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	public static int findMin(int h, int w) {
+		int fCnt=0;
+		//시작이 w
+		for(int i=h; i<h+8; i++) {
+			for(int j=w; j<w+8; j++) {
+				if(i%2==0) {
+					if(j%2==0 && !arr[i][j].equals("W")) {
+						fCnt++;
+					}else if(j%2==1 && !arr[i][j].equals("B")) {
+						fCnt++;
+					}
+				}else {
+					if(j%2==1 && !arr[i][j].equals("W")) {
+						fCnt++;
+					}else if(j%2==0 && !arr[i][j].equals("B")) {
+						fCnt++;
+					}
+				}
+			}
+		}
+		//시작이 B
+		int sCnt=0;
+		for(int i=h; i<h+8; i++) {
+			for(int j=w; j<w+8; j++) {
+				if(i%2==0) {
+					if(j%2==1 && !arr[i][j].equals("W")) {
+						sCnt++;
+					}else if(j%2==0 && !arr[i][j].equals("B")) {
+						sCnt++;
+					}
+				}else {
+					if(j%2==0 && !arr[i][j].equals("W")) {
+						sCnt++;
+					}else if(j%2==1 && !arr[i][j].equals("B")) {
+						sCnt++;
+					}
+				}
+			}
+		}
+		
+		return Math.min(fCnt, sCnt);
+	}
+	
+	public static void main(String[]args) throws Exception{
+		
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st=new StringTokenizer(br.readLine());
-		int height=Integer.parseInt(st.nextToken());
-		int width=Integer.parseInt(st.nextToken());
-		min=height*width;
-		String[][] arr=new String[height][width];
-		for(int i=0; i<height;i++) {
-			String tmp=br.readLine();
-			for(int j=0; j<width; j++) {
-				arr[i][j]=tmp.charAt(i)+"";
+		int h=Integer.parseInt(st.nextToken());
+		int w=Integer.parseInt(st.nextToken());
+		arr=new String[h][w];
+		for(int i=0; i<h; i++) {
+			arr[i]=br.readLine().split("");
+		}
+		int cnt=0;
+		for(int i=0; i<=h-8; i++) {
+			for(int j=0; j<=w-8; j++) {
+				answer=Math.min(answer, findMin(i, j));
+				cnt++;
 			}
 		}
+//		System.out.println(cnt);
+//		check(arr);
+		System.out.println(answer);
 		
-		for(int i=0; i<height-8; i++) {
-			for(int j=0; j<width-8; j++) {
-				dfs(arr,i,j,0);
-			}
-		}
-		
-		
-	}
-	public static void dfs(String[][] arr, int x, int y, int cnt) {
-		if(x==x+8 && y==y+8) {
-			if(cnt<min) {
-				min=cnt;
-				return;
-			}
-			return;
-		}
-		
-		for(int i=0; i<4; i++) {
-			int x_=x+dx[i];
-			int y_=y+dy[i];
-			if(x_>=0 && y_>=0 && x_<x+8 && y_<y+8) {
-				
-			}
-			
-		}
-		
-		
-	}
 
+	}
 }
