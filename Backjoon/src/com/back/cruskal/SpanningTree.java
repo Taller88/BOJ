28f82901c0baefe484b686ad5041185be6a7ebeb
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2020-08-24 15:37:04 +0900

    백준 최소스패닝 트리 문제풀이

--- Backjoon/src/com/back/cruskal/SpanningTree.java
+++ Backjoon/src/com/back/cruskal/SpanningTree.java
@@ -2,10 +2,23 @@ package com.back.cruskal;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class SpanningTree {
-
+	static int[]parent;
+	static class Node implements Comparable<Node>{
+		int s, a,cost;
+		public Node(int s, int a, int cost) {
+			this.s=s;
+			this.a=a;
+			this.cost=cost;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return this.cost-o.cost;
+		}
+	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 //		3 3
@@ -14,34 +27,45 @@ public class SpanningTree {
 //		1 3 3
 		StringTokenizer st=new StringTokenizer(br.readLine());
 		int n=Integer.parseInt(st.nextToken());
-		int[]parent=new int[n];
-		int[][] arr=new int[n+1][n+1];
+		parent=new int[n+1];
+		for(int i=0; i<parent.length; i++) {
+			parent[i]=i;
+		}
 		int vex=Integer.parseInt(st.nextToken());
+		PriorityQueue<Node> pq=new PriorityQueue<>();
 		for(int i=0; i<vex; i++) {
 			st=new StringTokenizer(br.readLine());
 			int s=Integer.parseInt(st.nextToken());
-			int d=Integer.parseInt(st.nextToken());
-			int w=Integer.parseInt(st.nextToken());
-			arr[s][d]=w;
-			unionParent(parent, s, d);
+			int a=Integer.parseInt(st.nextToken());
+			int cost=Integer.parseInt(st.nextToken());
+			pq.add(new Node(s, a, cost));
 		}
-	}
-	public static int getParent(int[] arr, int x) {
-		if(arr[x]==x) {
-			return x;
+		int result=0;
+		for(int i=0; i<vex; i++) {
+			Node node=pq.poll();
+			if(find(node.s)==find(node.a)) {
+				continue;
+			}
+			unionParent(node.s, node.a);
+			result+=node.cost;
 		}
-		return arr[x]=getParent(arr, arr[x]);
+		System.out.println(result);
+		
 	}
-	public static int unionParent(int[] arr, int x, int y) {
-		int a=getParent(arr, x);
-		int b=getParent(arr, y);
-		return a>b?b:a;
+	public static void unionParent(int x, int y) {
+		int aRoot = find(x);
+        int bRoot = find(y);
+        
+        if(aRoot != bRoot) {
+            parent[aRoot] = y;
+        } else {
+            return;
+        }
+		
 	}
-	public static boolean findParent(int[] arr, int x, int y) {
-		int a=getParent(arr, x);
-		int b=getParent(arr, y);
-		return a==b?true:false;
-	}
-	
-	
+	public static int find(int a) {
+		if(a == parent[a]) return a;
+        parent[a] = find(parent[a]);
+        return parent[a];
+    }
 }
