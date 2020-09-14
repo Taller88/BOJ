9ac95a52e70b70dcc7731a6c511423534167d73a
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2020-09-14 14:59:40 +0900

    Cos Pro 미완 문제 2번 n-소용돌이

--- Backjoon/src/com/cos/one/Test2.java
+++ Backjoon/src/com/cos/one/Test2.java
@@ -9,7 +9,11 @@ public class Test2 {
 	private static int solution(int num) {
 		int sum=0;
 		int[][]arr=new int[num][num];
-		
+//		0,0 -> 0,1 -> 0,2 -> 1,2 -> 2,2 -> 2,1 -> 2,0 -> 1,0 -> 1,1
+		//f(1)- 1
+		//f(2)- 1,3 ->4
+		//f(3)- 1,5,9 ->15
+		//f(4)- 1,7,13,15-> 36
 		return sum;
 	}
 
